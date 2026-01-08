b99b9fd2d3f768a11945a1079d5beca75a4c99f1
sarwagyashahonline <sarwagyashahonline@gmail.com>
2026-01-08 18:29:33 +0530

    Added some configurations

--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -25,3 +25,26 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False) -> N
             yaml.dump(content, file)
     except Exception as e:
         raise NetworkSecurityException(e, sys)
+    
+def save_numpy_array_data(file_path:str,array:np.array):
+    """SAve numpy array data to file
+    file_path:str location of file to save
+    array: np.array data to save"""
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,'wb') as file_object:
+            np.save(file_object,array)
+
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+    
+def save_object(file_path:str, obj:object)->None:
+    try:
+        logging.info("Entered the save object method of main utils class")
+        os.makedirs(os.path.dirname(file_path),exist_ok=True)
+        with open(file_path,'wb')as file_object:
+            pickle.dump(obj,file_object)# object stored as pickel file and later may be used as pickle.dump
+        logging.info("Exited the save object method of the main utils class")
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
