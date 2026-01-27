c98cb6349a1d830a188fd778c2aadbb8e3cb392e
sarwagyashahonline <sarwagyashahonline@gmail.com>
2026-01-28 01:11:44 +0530

    app.py chnaged

--- app.py
+++ app.py
@@ -84,5 +84,5 @@ async def predict_route(request: Request,file: UploadFile = File(...)):
             raise NetworkSecurityException(e,sys)
 
     
-if __name__=="__main__":
+if __name__=="__main__":### made some vhnageds
     app_run(app,host="0.0.0.0",port=8080)
